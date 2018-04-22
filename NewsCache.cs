d5bd114dca5d5d620c149dbd0d9f7ec8f2ecb92a
zielonykurczak <m.kowalski@mail.com>
2018-04-22 20:44:13 +0100

    Add files via upload

---
+++ NewsCache.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+
+namespace BBCparser
+{
+    public class NewsCache
+    {
+        private readonly Dictionary<string, DateTime> _newsCache;
+
+        public NewsCache()
+        {
+            _newsCache = new Dictionary<string, DateTime>();
+            LoadPreviousJson();
+        }
+
+        public void UpdateCache(string title, DateTime date)
+        {
+            _newsCache.Add(title, date);
+        }
+
+        public bool IsAlreadySaved(string title, DateTime date)
+        {
+            if (_newsCache.TryGetValue(title, out var dateFromCache))
+                return dateFromCache.Equals(date);
+            return false;
+        }
+
+        public void LoadPreviousJson()
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "feed", JsonGenerator.CurrentDateTime());
+            if (Directory.Exists(path) && Directory.EnumerateFiles(path).Any())
+                foreach (var file in Directory.EnumerateFiles(path))
+                {
+                    var json = File.ReadAllText(file);
+                    var jsonObject = JObject.Parse(json);
+                    foreach (var item in jsonObject["items"])
+                    {
+                        var date = item["pubDate"];
+                        var formattedDate = DateFormatter.ChangePubdateFormat(date.ToString());
+                        if (!_newsCache.ContainsKey(item["title"].ToString()))
+
+                            try
+                            {
+                                UpdateCache(item["title"].ToString(), formattedDate.Date);
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine(e);
+                                throw;
+                            }
+                    }
+                }
+        }
+    }
+}
